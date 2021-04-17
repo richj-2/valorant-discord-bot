25efba1d7c77c2a6bf3ee210cbb63312bca6f6fc
AlkyoneAoide <playinggameswithfriends@gmail.com>
2021-04-17 08:55:33 -0600

    fixed headless errors

--- bot.py
+++ bot.py
@@ -76,8 +76,12 @@ def leaderboard_get(page):
 #------------------Find player using webscraper--
 def find_player(player_name):
     #firefox setup
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     options = webdriver.firefox.options.Options()
     options.headless = True
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument(f'user-agent={user_agent}')
     driver = webdriver.Firefox(executable_path = path, options = options)
 
     #opens up valorant leaderboards
